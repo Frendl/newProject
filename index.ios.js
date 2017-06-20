43426d3d2ce53072242c1d9e02441580c20a4e9d
Mark <broccoliexpress@gmail.com>
2017-06-20 12:04:26 -0700

    Inital commit
    First commit

---
+++ index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './App/app'
+
+export default class newProject extends Component {
+  render() {
+    return (
+      <View>
+        <App />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+//    height: null,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    flex: 1,
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('newProject', () => newProject);
+
+// style={styles.welcome}
