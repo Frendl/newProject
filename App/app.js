27389c9dc43d65c072b400e4e54ab14247fb3152
Mark <broccoliexpress@gmail.com>
2017-06-23 09:58:02 -0700

    Atempt to switch back to ListView instead of FlatList
    Very messy and not working. Rebuild incoming.

--- App/app.js
+++ App/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    FlatList,
+    ListView,
     StyleSheet,
     ActivityIndicator
 } from 'react-native';
@@ -12,19 +12,19 @@ export default class App extends Component {
         super()
         this.state = {
             isLoading: false
-            MarsImages: []
+//            MarsImages: []
         }
     }
     componentDidMount() {
         return fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + '999' + 'MAST' + '&api_key=' + 'a1vxn94JAg11UtnooLxGQKwbSYpk85ml24xtqYAB')
             .then((response) => response.json())
             .then((responseJson) => {
-                let ds = new Flatlist.MarsImages({ rowHasChanged: (r1, r2) => r1 !== r2 });
+                let ds = new ListView.MarsImages({ rowHasChanged: (r1, r2) => r1 !== r2 });
                 this.setState({
                     isLoading: false,
                     MarsImages: ds.cloneWithRows(responseJson.photos),
                 }, function () {
-                    // do something with new state
+//                    return (ListView.MarsImages)
                 });
             })
             .catch((error) => {
@@ -43,12 +43,27 @@ export default class App extends Component {
 
         return (
             <View style={{ flex: 1, marginTop: 20 }}>
-                <FlatList
-                    keyExtractor={item => item.guid}
-                    data={this.state.MarsImages}
-                    renderItem={({ item }) => <MarsImage data={item} />}
+                <ListView
+                dataSource={this.state.MarsImage}
+                renderRow={(rowData) => <Text>{rowData.photos.objects}</Text>}
                 />
             </View>
         );
     }
 }
+
+
+/*+
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
+        />
+
+
+
+                <FlatList
+                    keyExtractor={item => item.guid}
+                    data={this.state.MarsImages}
+                    renderItem={({ photos }) => <MarsImage data={photos} />}
+                />
